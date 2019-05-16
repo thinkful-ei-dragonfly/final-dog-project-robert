77104a6c556b7ddbc27989f8c43df350bf2021f3
Robert Wiggins <wigginsro11@gmail.com>
2019-05-16 16:37:59 -0400

    completely functional image display

--- index.js
+++ index.js
@@ -5,29 +5,87 @@
 
 const getDogs = (function() {
 
+  const RANDOM_DOGS_URL = 'https://dog.ceo/api/breeds/image/random/';
 
-  function getImages(url, input) {
-    console.log('do something');
+  function generateMultipleHTML(imageArr) {
+    let html = '';
+    for (let i = 0; i < imageArr.length; i++) {
+      html += `<img src="${imageArr[i]}">`;
+    }
+    
+    $('.results').html(html);
+    
   }
 
+  function generateSingleHTML(breedURLImage) {
+    
+    $('.results').html(`<img src="${breedURLImage}">`);
+    
+  }
+
+
+  function getRandImages(input) {
+
+    return fetch(RANDOM_DOGS_URL + input)
+      .then( response => {
+        
+        if (!response.ok) {
+          throw new Error('Fetch error requesting image data from server');
+        }
+        
+        return response.json();
+      })
+      .catch(error => console.log(error.message));
+
+
+  }
+
+
+
+
+  function getSingleBreedImage(breed) {
+
+    return fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+      .then( response => {
+        
+        if (!response.ok) {
+          throw new Error('Fetch error requesting image data from server');
+        }
+        
+        return response.json();
+      })
+      .catch(error => console.log(error.message));
+  }
 
   function handleInput() {
     
-    $('#multiple-random-images').onclick('$random-dogs', event => {
-      $(event).preventDefault();
-      
-      console.log('getHere');
+    $('#multiple-random-images').on('submit', event => {
+      event.preventDefault();
+      let input = event.currentTarget.number.value;
+      if (input === '') {
+        input = 3;
+      }
+      getRandImages(input)
+        .then(data => generateMultipleHTML(data.message));
 
-      // let inputRandomNum = $('#random-button').val();
-      console.log(numInput);
+
     });
 
-  }
+    // Handle single Breed image
+    $('#single-breed-image').on('submit', event => {
+      event.preventDefault();
+      let input = event.currentTarget.breed.value;
+      getSingleBreedImage(input)
+        .then(data => generateSingleHTML(data.message))
+        .catch(error => console.log(error.message));
 
+    }
+    );
+  }
   return {
-    handleInput,
+    handleInput: handleInput,
   };
 
 })();
 
-$(handleInput());
+$(getDogs.handleInput());
